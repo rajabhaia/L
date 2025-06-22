6627f627e63e57c105c7da094daae19f2eab7306
jkljggggg <bbbdaru@gmail.com>
2025-06-22 22:14:49 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mohammadkj12/M",
+    "https://github.com/jkljggggg/L",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
