b8608af7f7445b09e5866049fb301eb2a9eec47a
mohammadkj12 <fefewo8978@ethsms.com>
2025-06-22 21:29:07 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", 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
+    "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/aajabetaaaja")
